784a43064e269550269dd11516c3c5826e3800fc
Masud Shuvo <masud.shuvo@widespace.com>
2016-02-19 17:43:25 +0600

    Implement public methods fetchObjects, pickObject, markObjectAsFavorite. Also added favoriteObjects property which will return favorite list. Exposed MSCodingChallengeDelegate protocol method's.

--- MSChallenge/MSCodingChallenge.m
+++ MSChallenge/MSCodingChallenge.m
@@ -7,9 +7,85 @@
 //
 
 #import "MSCodingChallenge.h"
+#import "MSObjectDataService.h"
+#import "MSObject.h"
 
+@interface MSCodingChallenge ()
+
+@property (nonatomic, strong) NSMutableArray *objects;
+@property (nonatomic, strong) NSMutableArray *markedFavoriteObjects;
+@property (nonatomic, strong) MSObject *pickedObject;
+
+@end
 
 @implementation MSCodingChallenge
 
+#pragma mark - Initialize Method
+- (instancetype)initWithDelegate:(id<MSCodingChallengeDelegate>)codingChallengeDelegate
+{
+    self = [super init];
+    if (self) {
+        self.objects = [NSMutableArray array];
+        self.markedFavoriteObjects = [NSMutableArray array];
+        self.delegate = codingChallengeDelegate;
+    }
+    return self;
+}
+
+#pragma mark - Setters and Getters
+- (NSArray *)favoriteObjects
+{
+    return self.markedFavoriteObjects;
+}
+
+#pragma mark - methods
+- (void)fetchObjects
+{
+    __weak MSCodingChallenge *weakSelf = self;
+    [MSObjectDataService fetchObjectsWithCompletion:^(NSArray *objects, NSError *error) {
+        __strong MSCodingChallenge *strongSelf = weakSelf;
+        if (!error) {
+            [strongSelf.objects addObjectsFromArray:objects];
+            if ([strongSelf.delegate respondsToSelector:@selector(objectsReadyToPick:)]) {
+                [strongSelf.delegate objectsReadyToPick:strongSelf];
+            }
+        }
+        else {
+            if ([strongSelf.delegate respondsToSelector:@selector(codingChallenge:didFailWithError:)]) {
+                [strongSelf.delegate codingChallenge:strongSelf didFailWithError:error];
+            }
+        }
+    }];
+}
+
+- (MSObject *)pickObject
+{
+    if ([self.objects count] > 0) {
+        NSInteger randomIndex = [self randomIndexFrom:0 maxIndex:[self.objects count] - 1];
+        self.pickedObject = [self.objects objectAtIndex:randomIndex];
+        [self.objects removeObjectAtIndex:randomIndex];
+    }
+    else {
+        if ([self.delegate respondsToSelector:@selector(noObjectLeftToPick:)]) {
+            [self.delegate noObjectLeftToPick:self];
+        }
+        self.pickedObject = nil;
+    }
+    return self.pickedObject;
+}
+
+- (void)markObjectAsFavorite
+{
+    if (self.pickedObject) {
+        [self.markedFavoriteObjects addObject:self.pickedObject];
+    }
+    self.pickedObject = nil;
+}
+
+#pragma mark - Private methods
+- (NSInteger)randomIndexFrom:(NSInteger)minIndex maxIndex:(NSInteger)maxIndex
+{
+    return minIndex + arc4random_uniform ((uint32_t)(maxIndex - minIndex + 1));
+}
 
 @end
