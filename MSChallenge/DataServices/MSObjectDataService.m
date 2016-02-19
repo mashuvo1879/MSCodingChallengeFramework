a539044b0984aad3a95b91f7c8f7f944f6e28b1c
Masud Shuvo <masud.shuvo@widespace.com>
2016-02-19 17:35:33 +0600

    Added data service class to retrive server response.

---
+++ MSChallenge/DataServices/MSObjectDataService.m
@@ -0,0 +1,52 @@
+//
+//  MSObjectDataService.m
+//  MSCodingChallengeFramework
+//
+//  Created by Masud Shuvo on 2/18/16.
+//  Copyright © 2016 Masud Shuvo. All rights reserved.
+//
+
+#import "MSObjectDataService.h"
+#import "MSObject.h"
+
+static NSString *const codingChallengeApiUrl = @"https://private-d847e-demoresponse.apiary-mock.com/questions";
+
+@implementation MSObjectDataService
+
++ (void)fetchObjectsWithCompletion:(void (^)( NSArray *objects, NSError *error))completion {
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:codingChallengeApiUrl]];
+    [request setHTTPMethod:@"GET"];
+    
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler: ^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        if (error)
+        {
+            completion(nil, error);
+            return;
+        }
+        
+        NSError *jsonError;
+        NSArray *objectArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&jsonError];
+        
+        if(jsonError)
+        {
+            completion(nil, jsonError);
+            return;
+        }
+
+        NSMutableArray *mutableAdArray = [[NSMutableArray alloc] init];
+        
+        [objectArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            MSObject *object = [[MSObject alloc] initWithDictionary:obj];
+            [mutableAdArray addObject:object];
+        }];
+        completion(mutableAdArray, nil);
+    }];
+    [task resume];
+}
+
+@end
